f2c59af964d9f4073a36d1b9eed93ccde809dff2
Eden <edenthink@gmail.com>
2017-04-03 20:31:27 +1000

    黑屏啦！

--- js/plugins/TimeSystem.js
+++ js/plugins/TimeSystem.js
@@ -9,9 +9,10 @@
 
 var dayTimeSystemParams = PluginManager.parameters('TimeSystem');
 
-var DayTimeSystem = function() {};
-var _DayTimeSystem = new DayTimeSystem();
-DayTimeSystem.prototype.day = 1;
+var DayTimeSystem = function() {
+    this.day = 1;
+};
+var _dayTimeSystem = new DayTimeSystem();
 
 DayTimeSystem.prototype.getDay = function () {
     return this.day;
@@ -30,11 +31,13 @@ DayTimeSystem.prototype.onDayChange = function () {
 };
 
 //=============================================================================
-// Add my window to scene map when it start
+// Scene map
 var _Scene_Map_start = Scene_Map.prototype.start;
 Scene_Map.prototype.start = function() {
     _Scene_Map_start.call(this);
     this._day_Window = new Day_Window(0, 0);
+    _dayTimeSystem._day_Window = this._day_Window;
+    console.log("_day_Window defined");
     this.addWindow(this._day_Window);
 };
 
@@ -44,7 +47,19 @@ Scene_Map.prototype.update = function() {
     _Scene_Map_update.call(this);
     this._day_Window.refresh();
 };
-
+var alias_Game_Screen_startFadeOut = Game_Screen.prototype.startFadeOut;
+Game_Screen.prototype.startFadeOut = function(duration) {
+    alias_Game_Screen_startFadeOut.call(this, duration);
+    console.log("out called");
+    _dayTimeSystem._day_Window.hide();
+};
+var alias_Game_Screen_startFadeIn = Game_Screen.prototype.startFadeIn;
+Game_Screen.prototype.startFadeIn = function(duration) {
+    alias_Game_Screen_startFadeIn.call(this, duration);
+    _dayTimeSystem._day_Window.show();
+};
+//=============================================================================
+// Add my window to scene map when it start
 function Day_Window() {
     this.initialize.apply(this, arguments);
 }
@@ -62,19 +77,22 @@ Day_Window.prototype.initialize = function(x, y) {
 
 // My window update function
 Day_Window.prototype.refresh = function() {
-    if (this._lastDay === _DayTimeSystem.day) {
+    if (this._lastDay === _dayTimeSystem.day) {
         return;
     }
     this.contents.clear();
     //Update
-    var day_text = '第' + String(_DayTimeSystem.day) + '天';
+    var day_text = '第' + String(_dayTimeSystem.day) + '天';
     this.contents.drawText(day_text, 10, 0, 80, 40, 'left');
 };
 
 var _alias_onDayChange = DayTimeSystem.prototype.onDayChange;
 DayTimeSystem.prototype.onDayChange = function () {
     _alias_onDayChange.call(this);
-    this.hide();
-    $gamePlayer.wait(300);
-    this.show();
-}
+    console.log("hide Called");
+    // .prototype._day_Window.hide();
+    // $gamePlayer.wait(300);
+    // Scene_Map.prototype._day_Window.show();
+};
+
+//=============================================================================
