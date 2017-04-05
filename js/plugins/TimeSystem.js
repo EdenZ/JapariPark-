9ed78fd8ae7edf5c72de6836d45b5592e169046b
Eden <edenthink@gmail.com>
2017-04-05 19:37:30 +1000

    时间细节

--- js/plugins/TimeSystem.js
+++ js/plugins/TimeSystem.js
@@ -10,6 +10,8 @@
 
 //=============================================================================
 //Constant
+//=============================================================================
+
 //色调信息
 var GAME_TIME_TINTS = [
     [30, 0, 40, 165], 	// => 0 hour
@@ -40,18 +42,27 @@ var GAME_TIME_TINTS = [
 //室内地图ID
 var INDOOR_MAPID = [3, 4, 6];
 
+var WEEKDAY_NAME = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+var MONTH_NAME = ['十二月', '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月'];
+
 //=============================================================================
 //Core basic
+//=============================================================================
 var dayTimeSystemParams = PluginManager.parameters('TimeSystem');
 
 function DayTimeSystem () {
     this.day = 1;
     this.hour = 6;
+    this.minute = 0;
+    this.week = 1;
+    this.month = 1;
+    this.year = 1;
 }
 var _dayTimeSystem = new DayTimeSystem();
 
 //=============================================================================
 //Timer
+//=============================================================================
 /**
  * 内部计时器
  * @constructor
@@ -59,24 +70,36 @@ var _dayTimeSystem = new DayTimeSystem();
 function SystemTimer() {
     this._work = false;
     this._count = 0;
-    this.start = function () {
-        if (_dayTimeSystem._timer._work) {
-            _dayTimeSystem._timer._count++;
-            _dayTimeSystem._timer.onTimeChange();
-        }
-        setTimeout(_dayTimeSystem._timer.start, 100);
-    };
-    this.onTimeChange = function () {
-        if (this._count >= 60) {
-            _dayTimeSystem.processHour();
-            this._count = 0;
-        }
-    };
 }
+
+/**
+ * 计时
+ */
+SystemTimer.prototype.start = function () {
+    //计时器在用 and 无对话窗口
+    if (_dayTimeSystem._timer._work && !$gameMessage.isBusy()) {
+        _dayTimeSystem._timer._count++;
+        _dayTimeSystem._timer.onTimeChange();
+    }
+    setTimeout(_dayTimeSystem._timer.start, 100);
+};
+
+/**
+ * 毫秒改变事件
+ */
+SystemTimer.prototype.onTimeChange = function () {
+    if (this._count >= 10) {
+        _dayTimeSystem.processMinute();
+        this._count = 0;
+    }
+};
+
 _dayTimeSystem._timer = new SystemTimer();
 var init_game_start = true;
 _dayTimeSystem._timer.start();
 
+//-----------------------------------------------------------------------------
+//GameMap
 //-----------------------------------------------------------------------------
 /**
  * 1.start timer when begin
@@ -99,8 +122,60 @@ Game_Map.prototype.setup = function (mapId) {
     }
 };
 
-//-----------------------------------------------------------------------------
+//=============================================================================
+//Year
+//=============================================================================
+
+DayTimeSystem.prototype.getYear = function () {
+    return this.year;
+};
+
+DayTimeSystem.prototype.calculateYear = function () {
+    this.year = Math.ceil(this.day / 84);
+    this.onYearChange();
+};
+
+DayTimeSystem.prototype.onYearChange = function () {
+    //Event
+};
+
+//=============================================================================
+//Month
+//=============================================================================
+
+DayTimeSystem.prototype.getMonth = function () {
+    return this.month;
+};
+
+DayTimeSystem.prototype.calculateMonth = function () {
+    this.month = Math.ceil(this.day / 7 ) % 12;
+    this.onMonthChange();
+};
+
+DayTimeSystem.prototype.onMonthChange = function () {
+    //Event
+};
+
+//=============================================================================
+//Week
+//=============================================================================
+
+DayTimeSystem.prototype.getWeek = function () {
+    return this.week;
+};
+
+DayTimeSystem.prototype.calculateWeek = function () {
+    this.week = this.day % 7;
+    this.onWeekChange();
+};
+
+DayTimeSystem.prototype.onWeekChange = function () {
+    //Event
+};
+
+//=============================================================================
 //Day
+//=============================================================================
 /**
  * 获取当前日期
  * @returns {number}
@@ -114,14 +189,26 @@ DayTimeSystem.prototype.getDay = function () {
  */
 DayTimeSystem.prototype.processDate = function () {
     this.day++;
+    this.calculateCalendar();
     this.hour = 0;
     this.onDayChange();
 };
 
+DayTimeSystem.prototype.calculateCalendar = function () {
+    this.calculateWeek();
+    this.calculateMonth();
+    this.calculateYear();
+};
+
 DayTimeSystem.prototype.passDayBySleep = function () {
+    if (this.hour >= 6) {
+        this.day++;
+        this.calculateCalendar();
+        this.onDayChange();
+    }
     this.hour = 6;
-    this.processDate();
-}
+    this._timer._count = 0;
+};
 
 /**
  * 日期变更事件
@@ -131,6 +218,7 @@ DayTimeSystem.prototype.onDayChange = function () {
 };
 //=============================================================================
 //Hour
+//=============================================================================
 /**
  * 获取小时
  * @returns {number}
@@ -143,10 +231,10 @@ DayTimeSystem.prototype.getHour = function () {
  * 小时++
  */
 DayTimeSystem.prototype.processHour = function () {
-    if (this.hour >= 23) {
+    this.hour++;
+    this.minute = 0;
+    if (this.hour >= 24) {
         this.processDate();
-    } else {
-        this.hour++;
     }
     this.onHourChange();
 };
@@ -170,8 +258,28 @@ DayTimeSystem.prototype.onHourChange = function () {
     }
 };
 
+//=============================================================================
+//Minute
+//=============================================================================
+DayTimeSystem.prototype.getMinute = function () {
+    return this.minute;
+};
+
+DayTimeSystem.prototype.processMinute = function () {
+    this.minute++;
+    if (this.minute >= 60) {
+        this.processHour();
+    }
+    this.onMinuteChange();
+};
+
+DayTimeSystem.prototype.onMinuteChange = function () {
+    //Event
+};
+
 //-----------------------------------------------------------------------------
 // Scene map
+//-----------------------------------------------------------------------------
 /**
  * 添加时间窗口到左上角
  * @type {*}
@@ -199,25 +307,30 @@ Scene_Map.prototype.update = function () {
 
 /**
  * Hide day window when fade out
+ * 停止计时
  * @type {*}
  */
 var alias_Game_Screen_startFadeOut = Game_Screen.prototype.startFadeOut;
 Game_Screen.prototype.startFadeOut = function (duration) {
     alias_Game_Screen_startFadeOut.call(this, duration);
     _dayTimeSystem._day_Window.hide();
+    _dayTimeSystem._timer._work = false;
 };
 
 /**
  * Show day window when fade out
+ * 开始计时
  * @type {*}
  */
 var alias_Game_Screen_startFadeIn = Game_Screen.prototype.startFadeIn;
 Game_Screen.prototype.startFadeIn = function (duration) {
     alias_Game_Screen_startFadeIn.call(this, duration);
     _dayTimeSystem._day_Window.show();
+    _dayTimeSystem._timer._work = true;
 };
-//-----------------------------------------------------------------------------
-// Add my window to scene map when it start
+//=============================================================================
+//时间窗口
+//=============================================================================
 function Day_Window() {
     this.initialize.apply(this, arguments);
 }
@@ -230,11 +343,11 @@ Day_Window.prototype.constructor = Day_Window;
  * @param y
  */
 Day_Window.prototype.initialize = function (x, y) {
-    Window_Base.prototype.initialize.call(this, x, y, 240, 80);
+    Window_Base.prototype.initialize.call(this, x, y, 200, 120);
     //this.drawIcon(...);
     //this.drawText(...);
     //...
-    this._lastDay = 0;
+    this._lastMinute = -1;
     this._lastHour = -1;
     this.refresh();
 };
@@ -244,15 +357,19 @@ Day_Window.prototype.initialize = function (x, y) {
  * 刷新窗口信息
  */
 Day_Window.prototype.refresh = function () {
-    if (this._lastDay !== _dayTimeSystem.day || this._lastHour !== _dayTimeSystem.getHour()) {
-        this._lastDay = _dayTimeSystem.getDay();
+    if (this._lastHour !== _dayTimeSystem.getHour() || this._lastMinute !== _dayTimeSystem.getMinute()) {
+        this._lastMinute = _dayTimeSystem.getMinute();
         this._lastHour = _dayTimeSystem.getHour();
         this.contents.clear();
         //Update
-        var day_text = '第' + String(_dayTimeSystem.day) + '天';
-        var hour_text = String(_dayTimeSystem.getHour()) + ' 点';
-        this.contents.drawText(day_text, 0, 0, 70, 40, 'left');
-        this.contents.drawText(hour_text, 120, 0, 70, 40, 'left');
+        var calendar_text = String(_dayTimeSystem.year) + '年  ' + MONTH_NAME[_dayTimeSystem.month];
+        var weekName_text = WEEKDAY_NAME[_dayTimeSystem.week];
+        var hour = (_dayTimeSystem.getHour() < 10 ? '0' : '') + String(_dayTimeSystem.getHour());
+        var minute = (_dayTimeSystem.getMinute() < 10 ? '0' : '') + String(_dayTimeSystem.getMinute());
+        var clock_text = hour + ':' + minute;
+        this.contents.drawText(calendar_text, 10, 0, 180, 40, 'left');
+        this.contents.drawText(weekName_text, 10, 40, 50, 40, 'left');
+        this.contents.drawText(clock_text, 80, 40, 120, 40, 'left');
     }
 };
 
@@ -265,5 +382,3 @@ var _day_window_onDayChange = DayTimeSystem.prototype.onDayChange;
 DayTimeSystem.prototype.onDayChange = function () {
     _day_window_onDayChange.call(this);
 };
-
-//=============================================================================
