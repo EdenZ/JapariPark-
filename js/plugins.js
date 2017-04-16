ffcdc84fec472b691343918ebd9494a9c1d473f3
shimoku <takeshi_ou@yahoo.com>
2017-04-16 20:55:04 +0800

    1

--- js/plugins.js
+++ js/plugins.js
@@ -5,5 +5,6 @@ var $plugins =
 {"name":"FriendCore","status":true,"description":"Friend System Core","parameters":{"debug":"false"}},
 {"name":"TimeSystem","status":true,"description":"Time system","parameters":{"debug":"false"}},
 {"name":"FriendStateSystem","status":true,"description":"Friend State Controller","parameters":{"debug":"false"}},
-{"name":"FarmingSystem","status":true,"description":"Farm System","parameters":{"debug":"false"}}
+{"name":"FarmingSystem","status":true,"description":"Farm System","parameters":{"debug":"false"}},
+{"name":"CookingSystem","status":true,"description":"Cooking System","parameters":{"debug":"false"}}
 ];
